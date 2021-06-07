d5087169db8664e6e1f1f0b78b600425d920dbb0
Lukas Bohman <lukas-bohman@hotmail.com>
2021-06-07 13:33:37 +0200

    second method

--- src/libapp/Main.java
+++ src/libapp/Main.java
@@ -2,14 +2,25 @@ package libapp;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Main test = new Main();
+        int add = test.myMethod(5);
+        System.out.println(add);
+
+        Main test2 = new Main();
+        int add2 = test2.myMethod2(5);
+        System.out.println("\n" + add2);
+    }
+
+//Incrementing
     public int myMethod(int num) {
         int increment = num + 1;
         return increment;
     }
 
-    public static void main(String[] args) {
-        Main test = new Main();
-        int add = test.myMethod(5);
-        System.out.println(add);
+//Decrementing
+    public int myMethod2(int num) {
+        int decrement = num - 1;
+        return decrement;
     }
 }
