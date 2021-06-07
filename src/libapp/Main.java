96bbe7d9a42fd133eb6cf36e9fcc2a73b5a186b8
Lukas Bohman <lukas-bohman@hotmail.com>
2021-06-07 14:57:40 +0200

    multiplication method

--- src/libapp/Main.java
+++ src/libapp/Main.java
@@ -7,9 +7,18 @@ public class Main {
         return increment;
     }
 
+    public int myMethod3(int x, int y) {
+        int z = x *y;
+        return z;
+    }
+
     public static void main(String[] args) {
         Main test = new Main();
         int add = test.myMethod(5);
         System.out.println(add);
+
+        Main test3 = new Main();
+        int add3 = test3.myMethod3(5, 5);
+        System.out.println(add3);
     }
 }
